59de1aa1c110ea21a3a45d8337f92e60929f0da7
Austin He <ahe8@my.bcit.ca>
2023-07-18 11:35:13 -0700

    fix css for login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { LoginComponent } from "../components/LoginComponent"
 import { RegisterComponent } from "../components/RegisterComponent"
+import "./Login.css"
 
 export default function Login() {
     const [currentForm, setCurrentForm] = useState("login");
