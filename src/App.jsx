dbfe7052f8fe4ff8e185347c6596b29bdbe508f7
livictor888 <victorli@live.ca>
2023-07-18 10:55:20 -0700

    Register and Login

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
 import { useState } from 'react'
 import './App.css'
 import Calendar from "./components/Calendar"
+import { Register } from "./user/Register";
+import { Login } from "./user/Login";
 
 function App() {
+  // return (
+  //   <>
+  //     <Login />
+  //     <Calendar />
+  //   </>
+  // )
+
+  const [currentForm, setCurrentForm] = useState("login");
+  const toggleForm = (forName) => {
+      setCurrentForm(forName);
+  }
   return (
-    <>
-      <Calendar />
-    </>
-  )
+      <div className="App">
+          {
+              currentForm === "login" ? <Login onFormSwitch={toggleForm} /> : <Register onFormSwitch={toggleForm}/>
+          }
+      </div>
+  );
 }
 
 export default App
---
+++ src/user/App.jsx
@@ -0,0 +1,30 @@
+// unsure where this code should go
+
+// import { useState } from 'react'
+// import './App.css'
+// import Calendar from "./components/Calendar"
+// import { Register } from "./user/Register";
+// import { Login } from "./user/Login";
+
+// function App() {
+//   // return (
+//   //   <>
+//   //     <Login />
+//   //     <Calendar />
+//   //   </>
+//   // )
+
+//   const [currentForm, setCurrentForm] = useState("login");
+//   const toggleForm = (forName) => {
+//       setCurrentForm(forName);
+//   }
+//   return (
+//       <div className="App">
+//           {
+//               currentForm === "login" ? <Login onFormSwitch={toggleForm} /> : <Register onFormSwitch={toggleForm}/>
+//           }
+//       </div>
+//   );
+// }
+
+// export default App
