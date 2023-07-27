bdc7f3b6ca3d21cc86fa14bdc2185b0dee9b2dd3
livictor888 <victorli@live.ca>
2023-07-27 04:07:45 -0700

    Firebase initialize

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
-import './App.css'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from "./pages/Login"
-import Layout from "./components/Layout"
-import dotenv from 'dotenv';
-
-// Load environment variables from the .env file
-dotenv.config();
+import React, { useEffect } from 'react';
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import Layout from './components/Layout';
+import firebase from 'firebase/app';
+import 'firebase/database'; // Import other Firebase services you plan to use
 
 function App() {
-
-  // Access the environment variables
+  // Your Firebase project config
   const firebaseConfig = {
     apiKey: process.env.VITE_FIREBASE_KEY,
     authDomain: process.env.VITE_FIREBASE_AUTH_DOMAIN,
@@ -18,18 +16,24 @@ function App() {
     messagingSenderId: process.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
     appId: process.env.VITE_FIREBASE_APP_ID,
   };
-  
+
+  // Initialize Firebase only once
+  useEffect(() => {
+    if (!firebase.apps.length) {
+      firebase.initializeApp(firebaseConfig);
+    }
+  }, [firebaseConfig]);
+
   return (
     <>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Layout />} />
           <Route path="/login" element={<Login />} />
-          
         </Routes>
       </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
