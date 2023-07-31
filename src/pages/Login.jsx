c35ceea37cde992a5c5e9b9d8a7ca0d83e4e01eb
Austin He <ahe8@my.bcit.ca>
2023-07-31 12:56:46 -0700

    add landing page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,9 +2,11 @@ import { useState } from 'react'
 import { LoginComponent } from "../components/LoginComponent"
 import { RegisterComponent } from "../components/RegisterComponent"
 import "./Login.css"
+import { useLocation } from 'react-router-dom'
 
 export default function Login() {
-    const [currentForm, setCurrentForm] = useState("login");
+    const [currentForm, setCurrentForm] = useState(useLocation().state ? "register" : "login");
+
     const toggleForm = (forName) => {
         setCurrentForm(forName);
     }
