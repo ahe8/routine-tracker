51de2fde17f8794b6f36faa7d8b498963eece86b
livictor888 <victorli@live.ca>
2023-08-09 02:07:03 -0700

    Add env values for db.js

--- server/db.js
+++ server/db.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const Pool = require("pg").Pool;
 
 // const pool = new Pool({
@@ -8,12 +9,18 @@ const Pool = require("pg").Pool;
 //   database: "routine_database",
 // });
 
+// console.log("DB Username:", process.env.REACT_APP_DB_USERNAME);
+// console.log("DB Password:", process.env.REACT_APP_DB_PASSWORD);
+// console.log("DB Port:", process.env.REACT_APP_DB_PORT);
+// console.log("DB Database Name:", process.env.REACT_APP_DB_DATABASE);
+
 const pool = new Pool({
-    user: "postgres",
-    password: "password",
-    host: "localhost",
-    port: 5432,
-    database: "routine_database",
+    user: process.env.REACT_APP_DB_USERNAME,
+    password: process.env.REACT_APP_DB_PASSWORD,
+    host: process.env.REACT_APP_HOST,
+    port: process.env.REACT_APP_DB_PORT,
+    database: process.env.REACT_APP_DB_DATABASE,
   });
+  
 
 module.exports = pool;
