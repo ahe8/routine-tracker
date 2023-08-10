57505cf35e6b561dd1e8a49baf540cd6433ea68c
Austin He <ahe8@my.bcit.ca>
2023-08-10 12:19:05 -0700

    fix unresolved merge conflict

--- src/components/Habits.jsx
+++ src/components/Habits.jsx
@@ -70,12 +70,7 @@ export default function Habits() {
     }
   }
 
-<<<<<<< HEAD
   
-  let habitElements = habits
-    .filter((habit) => habit["routine_yyyymm"] === getYYYYMM(date))
-    .map((habit) => <Habit key={habit["routine_id"]} {...habit} />);
-=======
   async function handleDelete(routineId) {
     try {
       await fetch(`http://localhost:5001/${currUser.uid}/routines`, {
@@ -152,7 +147,6 @@ export default function Habits() {
         )}
       </div>
     ));
->>>>>>> main
 
   return (
     <>
